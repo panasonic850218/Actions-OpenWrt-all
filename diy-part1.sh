067bbf23814de88de9779d7c6921921aaf099f32
ZJJCKA <96774959+ZJJCKA@users.noreply.github.com>
2025-01-20 15:03:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,13 +16,4 @@
 # Add a feed source
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
-rm -rf feeds/packages/utils/v2dat
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-./scripts/feeds install -a 
-git clone https://github.com/Siriling/5G-Modem-Support.git feeds/packages/5G-Modem-Support
-git clone https://github.com/vernesong/OpenClash.git feeds/packages/OpenClash
-git clone https://github.com/sirpdboy/netspeedtest.git feeds/packages/netspeedtest
-sed -i '3i echo 60 > /sys/class/hwmon/hwmon1/pwm1' package/base-files/files/etc/rc.local
+
