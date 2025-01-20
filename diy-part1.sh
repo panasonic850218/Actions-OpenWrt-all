ac57e1bc4b2c6459c73340072293803573f774ed
ZJJCKA <96774959+ZJJCKA@users.noreply.github.com>
2025-01-20 15:41:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,5 +17,7 @@
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '3i src-git-full modem https://github.com/ZJJCKA/5G-Modem-Support.git
+./scripts/feeds update -a
+./scripts/feeds install -a
 
 
